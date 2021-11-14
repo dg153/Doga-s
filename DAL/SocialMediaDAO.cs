aafd834de20c743620b7df8522e21ba6480673c3
Doğa Can ÖZER <34800762+dg153@users.noreply.github.com>
2021-11-14 23:54:29 +0300

    Add project files.

---
+++ DAL/SocialMediaDAO.cs
@@ -0,0 +1,79 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DAL
+{
+    public class SocialMediaDAO : PostContext
+    {
+        public int AddSocialMedia(SocialMedia social)
+        {
+            try
+            {
+                db.SocialMedia.Add(social);
+                db.SaveChanges();
+                return social.ID;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public List<SocialMediaDTO> GetSocialMedias()
+        {
+            List<SocialMedia> list = db.SocialMedia.Where(x => x.isDeleted == false).ToList();
+            List<SocialMediaDTO> dtolist = new List<SocialMediaDTO>();
+            foreach (var item in list)
+            {
+                SocialMediaDTO dto = new SocialMediaDTO();
+                dto.Name = item.Name;
+                dto.Link = item.Link;
+                dto.ImagePath = item.ImagePath;
+                dto.ID = item.ID;
+                dtolist.Add(dto);
+
+            }
+            return dtolist;
+        }
+
+        public SocialMediaDTO GetSocialMediaWithID(int ID)
+        {
+            SocialMedia socialmedia = db.SocialMedia.First(x => x.ID == ID);
+            SocialMediaDTO dto = new SocialMediaDTO();
+            dto.ID = socialmedia.ID;
+            dto.Name = socialmedia.Name;
+            dto.Link = socialmedia.Link;
+            dto.ImagePath = socialmedia.ImagePath;
+            return dto;
+        }
+
+        public string UpdateSocialMedia(SocialMediaDTO model)
+        {
+            try
+            {
+                SocialMedia social = db.SocialMedia.First(x => x.ID == model.ID);
+                string oldImagePath = social.ImagePath;
+                social.Name = model.Name;
+                social.Link = model.Link;
+                if (model.ImagePath != null)
+                    social.ImagePath = model.ImagePath;
+                //social.ImagePath = model.ImagePath;
+                social.LastUpdateDate = DateTime.Now;
+                social.LastUpdateUserID = UserStatic.UserID;
+                db.SaveChanges();
+                return oldImagePath;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
---
+++ UI/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DTO;
+using BLL;
+
+
+namespace UI.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        UserBLL userbll = new UserBLL();
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            UserDTO dto = new UserDTO();
+            return View(dto);
+        }
+        [HttpPost]
+        public ActionResult Index(UserDTO model)
+        {
+            /*var errors = ModelState
+    .Where(x => x.Value.Errors.Count > 0)
+    .Select(x => new { x.Key, x.Value.Errors })
+    .ToArray();*/
+            if (!ModelState.IsValid)
+            {
+                //return model.RequiredAttribute();
+                UserDTO user = userbll.GetUserWithUsernameAndPassword(model);
+                if (user.ID != 0)
+                {
+                    UserStatic.UserID = user.ID;
+                    UserStatic.isAdmin = user.IsAdmin;
+                    UserStatic.NameSurname = user.Name;
+                    UserStatic.ImagePath = user.ImagePath;
+                    LogBLL.AddLog(General.ProcessType.Login,General.TableName.Login, 12);
+                    return RedirectToAction("Index", "Post");
+
+                }
+                else
+                //Console.WriteLine(errors);
+                return View(model);   
+            }
+            else
+                return View(model);
+        }
+
+    }
+}
